b16dfec3e9933ee70e87a75b7b593d9755d088ec
Malik-AhmadSE <121057088+Malik-AhmadSE@users.noreply.github.com>
2024-09-05 04:26:08 +0500

    error 001 solve

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -5,7 +5,7 @@ import Footer from '../../components/footer/Footer';
 import ContactInfo from '../../components/ContactComponent/contactinfo/ContactInfo.jsx';
 import Form from '../../components/ContactComponent/contactinfo/Form.jsx';
 import ContactDiv from '../../components/ContactComponent/contactinfo/ContactDiv.jsx';
-import Location from '../../components/ContactComponent/Location/Location.jsx';
+import Location from '../../components/ContactComponent/location/Location.jsx';
 import Faq from '../../components/Common/faq/Faq.jsx';
 import CeoRating from '../../components/ContactComponent/ceorating/CeoRating.jsx';
 import Discover from '../../components/Common/discover/Discover.jsx';
