d6604df06bc4f2cf855d7f3568a5248c88f170a8
Malik-AhmadSE <121057088+Malik-AhmadSE@users.noreply.github.com>
2024-09-08 20:37:58 +0500

    change skeleton

--- src/pages/Allproducts/air/Air.jsx
+++ src/pages/Allproducts/air/Air.jsx
@@ -1,18 +1,20 @@
-// AirComponent.js
 import React, { useState, useEffect } from 'react';
-import { Card, Modal,Button } from 'antd';
+import { Card, Modal, Skeleton } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
 import { ImageImporter } from './ImageImporter/ImageImporter.jsx'; // Import the ImageImporter component
 const { Meta } = Card;
 
-
 const AirComponent = ({ open, handleCancel }) => {
   const [imageData, setImageData] = useState([]);
+  const [loading, setLoading] = useState(true); // Add loading state
 
   useEffect(() => {
-    // Fetch combined image and product details data
-    const data = ImageImporter();
-    setImageData(data);
+    // Simulate an API call or image loading
+    setTimeout(() => {
+      const data = ImageImporter();
+      setImageData(data);
+      setLoading(false); // Set loading to false after data is fetched
+    }, 2000); // Simulate a delay for loading
   }, []);
 
   return (
@@ -33,23 +35,35 @@ const AirComponent = ({ open, handleCancel }) => {
           padding: '20px',
         }}
       >
-        {imageData.map((card, index) => (
-          <Card
-            key={index}
-            hoverable
-            style={{
-              width: 240,
-              marginBottom: '16px',
-            }}
-            cover={<img alt={card.title} src={card.src} style={{ width: '100%' }} />}
-
-          >
-            <Meta title={card.title} description={card.description} />
-            {/* <Button type={text} onClick={() => sendMessage(card.title,card.description, '+971558531096')}>
-              Buy
-            </Button> */}
-          </Card>
-        ))}
+        {loading ? (
+          Array.from({ length: 10 }).map((_, index) => (
+            <Card
+              key={index}
+              hoverable
+              style={{
+                width: 240,
+                marginBottom: '16px',
+              }}
+            >
+              <Skeleton.Image style={{ width: '100%', height: '150px' }} />
+              <Skeleton active paragraph={{ rows: 2 }} />
+            </Card>
+          ))
+        ) : (
+          imageData.map((card, index) => (
+            <Card
+              key={index}
+              hoverable
+              style={{
+                width: 240,
+                marginBottom: '16px',
+              }}
+              cover={<img alt={card.title} src={card.src} style={{ width: '100%' }} />}
+            >
+              <Meta title={card.title} description={card.description} />
+            </Card>
+          ))
+        )}
       </div>
     </Modal>
   );
