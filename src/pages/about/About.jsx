b0e94f4169858ab083537f576baecdfb55fa80b3
Malik-AhmadSE <121057088+Malik-AhmadSE@users.noreply.github.com>
2024-09-27 12:47:05 +0500

    done

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -19,7 +19,7 @@ function About() {
       <div className='md:mt-60 pt-10'>
        <Serviceab />
       </div>
-      <div className='md:-mt-[100px] mt-[450px]'>
+      <div className='md:-mt-[100px] relative top-96'>
         <CeoRating />
       </div>
       <Footer />
