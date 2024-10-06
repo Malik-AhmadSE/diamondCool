4888900978a4a7321d664754911090ee467a3290
Malik-AhmadSE <121057088+Malik-AhmadSE@users.noreply.github.com>
2024-10-06 12:19:42 +0500

    changed

--- src/components/homeComponent/brands/brands.jsx
+++ src/components/homeComponent/brands/brands.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Flex, Typography, Image, Carousel } from 'antd'; 
 import { Tooltip } from 'antd';
 import {AmazonSquareFilled} from '@ant-design/icons';
+import Pic from '../../../assets/noon.svg';
 const imageUrls = [
   'https://res.cloudinary.com/dvf9mmcww/image/upload/v1728187682/1_htil0p.png',
   'https://res.cloudinary.com/dvf9mmcww/image/upload/v1728187683/2_p1o8t4.png',
@@ -11,14 +12,14 @@ const imageUrls = [
 function Brands() {
   return (
     <div className="!w-screen flex-col items-center justify-center mb-7">
-      
+      <Flex className='w-[90%] md:w-full flex bg-logo bg-cover justify-center items-center md:gap-52 gap-14 '>
+      <Tooltip title="Buy from noon" ><a href='https://www.noon.com/seller/p-149548/?offer_code=ze536cca4ff792a1163e9z-1&sku=ZE536CCA4FF792A1163E9Z'><Image alt='img' src={Pic} preview={false} className='md:!w-[250px] mt-1 !w-[200px]' /> </a></Tooltip>
+        <Tooltip title="Buy from Amazon" ><a href='https://www.amazon.ae/s?me=A1RG19Q6S3VY4C&marketplaceID=A2VIGQ35RCS4UG'> <AmazonSquareFilled  className='md:text-[110px] text-[90px] text-col'/></a></Tooltip>
+      </Flex>
       <Typography.Title className='text-[45px] font-bold text-center pt-10'>
         Brands
       </Typography.Title>
-      <Flex className='w-full flex bg-logo bg-cover justify-center items-center md:gap-40 gap-10 '>
-      <Tooltip title="Buy from noon" ><a href='https://www.noon.com/seller/p-149548/?offer_code=ze536cca4ff792a1163e9z-1&sku=ZE536CCA4FF792A1163E9Z'></a></Tooltip>
-        <Tooltip title="Buy from Amazon" ><a href='https://www.amazon.ae/s?me=A1RG19Q6S3VY4C&marketplaceID=A2VIGQ35RCS4UG'> <AmazonSquareFilled  className='text-[150px] text-col'/></a></Tooltip>
-      </Flex>
+      
       <Carousel autoplay speed={200} className="w-full" >
         {imageUrls.map((url, index) => (
           <div key={index} className="md:h-56 flex items-center justify-center">
